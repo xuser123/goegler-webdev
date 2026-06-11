7f1a0ca7f8254ea7b7de46643b49f6992142056a
Mike <jumi@jumi.biz>
2026-06-12 00:06:51 +0200

    rename nav links placeholder

--- www/app/Layout/header.php
+++ www/app/Layout/header.php
@@ -13,23 +13,33 @@
 <nav>
 <ul class="nav justify-content-end text-bg-primary p-2">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="/app/Pages/home">Home</a>
+    <a class="nav-link active" aria-current="page" 
+			href="/app/Pages/home">Home</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="/app/Pages/about.php">About Us</a>
   </li>
 	<div class="dropdown">
-		<button class="btn btn-secondary dropdown-toggle" type="button" 
-			data-bs-toggle="dropdown" aria-expanded="false">Developement</button>
+		<button class="btn btn-secondary dropdown-toggle" 
+			type="button" 
+				data-bs-toggle="dropdown" 
+					aria-expanded="false">Developement</button>
 		<ul class="dropdown-menu">
-			<li><a class="dropdown-item" href="/app/Pages/blog">Agriculture</a></li>
-			<li><a class="dropdown-item" href="/app/Pages/dashboard">Industrial</a></li>
-			<li><a class="dropdown-item" href="/app/Pages/jumbotron">Photovoltaic</a></li>
-		  <li><a class="dropdown-item" href="/app/Pages/heroes">Partners</a></li>
-			<li><a class="dropdown-item" data-bs-target="#modalToggle1" 
-				data-bs-toggle="modal">Contact Us</a></li>
-			<li><a class="dropdown-item" href="/app/Pages/masonry">Gallery</a></li>
+			<li><a class="dropdown-item" 
+				href="/app/Pages/blog">Agriculture</a></li>
+			<li><a class="dropdown-item" 
+				href="/app/Pages/dashboard">Industrial</a></li>
+			<li><a class="dropdown-item" 
+				href="/app/Pages/jumbotron">Photovoltaic</a></li>
+		  <li><a class="dropdown-item" 
+				href="/app/Pages/heroes">Partners</a></li>
 		</ul>
+	</div>
+	<li><a class="dropdown-item" 
+		data-bs-target="#modalToggle1" 
+			data-bs-toggle="modal">Contact Us</a></li>
+	<li><a class="dropdown-item" 
+		href="/app/Pages/masonry">Gallery</a></li>
 	</div>
 </ul>
 </nav>
